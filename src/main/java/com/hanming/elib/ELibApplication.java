ecd18e1aa9a143bcb18b7d9d68fb933b7e51edf0
hanming.xiao <hanming.xiao@yeepay.com>
2019-09-30 15:44:19 +0800

    project

---
+++ src/main/java/com/hanming/elib/ELibApplication.java
@@ -0,0 +1,13 @@
+package com.hanming.elib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanming/elib/ELibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hanming.elib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ELibApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
